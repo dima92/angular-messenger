09b8b40d974af98ea7c1cde3898afbcd588eba50
dima92 <dima-tkachenko@mail.ru>
2022-08-29 13:10:42 +0300

    feat: workshop preparation

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  isLoggedIn = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
