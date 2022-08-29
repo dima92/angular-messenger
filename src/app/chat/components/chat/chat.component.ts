e84f10f41b2715649fd1682315845bf1f6cd860c
dima92 <dima-tkachenko@mail.ru>
2022-08-29 13:59:46 +0300

    feat: add autoscroll to bottom

--- src/app/chat/components/chat/chat.component.ts
+++ src/app/chat/components/chat/chat.component.ts
@@ -1,36 +1,40 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, EventEmitter, Input, Output, ViewChild} from '@angular/core';
 import {SelectedConversationModel} from '../../models/conversation.model';
 import {ConversationMessageModel, MessageSendRequestModel} from '../../models/message.model';
 import {UserModel} from '../../../core/models/user.model';
+import {ViewportScroller} from "@angular/common";
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
-  @Input() selectedConversation: SelectedConversationModel | undefined;
+export class ChatComponent implements AfterViewChecked {
+  @Input() selectedConversation!: SelectedConversationModel;
   @Input() currentUser: UserModel | null = null;
   @Output() postMessage = new EventEmitter<MessageSendRequestModel>();
 
+  @ViewChild('lastMessageAnchor') lastMessageAnchor!: ElementRef;
+  @ViewChild('scrollContainer') scrollContainer!: ElementRef;
+
   newMessage: string = '';
 
-  constructor() {
+  constructor(private scroller: ViewportScroller) {
   }
 
-  ngOnInit(): void {
+  ngAfterViewChecked() {
+    this.scrollContainer.nativeElement.scrollTo(0, this.scrollContainer.nativeElement.scrollHeight);
   }
 
   getUser(message: ConversationMessageModel) {
-    return this.selectedConversation?.participants.find(user => user.id === message.sender);
+    return this.selectedConversation.participants.find(user => user.id === message.sender);
   }
 
   onPostMessage() {
     this.postMessage.emit({
       message: this.newMessage,
-      recipient: this.selectedConversation?.participants[0].id || ''
+      recipient: this.selectedConversation.participants[0].id || ''
     });
     this.newMessage = '';
   }
-
 }
