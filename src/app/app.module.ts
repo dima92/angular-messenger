b2575212de76515c7bf3be53983b0e6d297267b7
dima92 <dima-tkachenko@mail.ru>
2022-08-29 12:19:57 +0300

    workshop: components, directives, pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {ConversationListComponent} from './components/conversation-list/conversation-list.component';
+import {ChatComponent} from './components/chat/chat.component';
+import {ChatMessageComponent} from './components/chat-message/chat-message.component';
+import {StatusDirective} from './directives/status.directive';
+import {FilterPipe} from './pipes/filter.pipe';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConversationListComponent,
+    ChatComponent,
+    ChatMessageComponent,
+    StatusDirective,
+    FilterPipe,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
